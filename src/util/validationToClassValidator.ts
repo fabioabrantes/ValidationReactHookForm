0ece2b0e0fa16f312e146ef0a83bb3fde00c58b7
fabio abrantes diniz <fabio.abrantes.diniz@gmail.com>
2022-12-22 09:36:13 -0300

    added coment in function validationToClassValidator

--- src/util/validationToClassValidator.ts
+++ src/util/validationToClassValidator.ts
@@ -1,3 +1,6 @@
+// tive que criar esse arquivo contendo este método para fazer um decorator
+// para validar o confirmPassword. Ele pega o valor do atributo password e verifica
+// se é igual ao artibuto confirmPassword
 import { registerDecorator, ValidationOptions, ValidationArguments } from 'class-validator';
 
 export function confirmPassword(property: string, validationOptions?: ValidationOptions) {
